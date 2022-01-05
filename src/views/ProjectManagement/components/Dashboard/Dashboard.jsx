ef7793cc54f95911a1659cef135187046a061b58
mahesh863 <maheshchoudhury135@gmail.com>
2022-01-05 22:38:04 +0530

    Updated

--- src/views/ProjectManagement/components/Dashboard/Dashboard.jsx
+++ src/views/ProjectManagement/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,40 @@
 import './Dashboard.scss';
 
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
 import React from 'react';
-
-import faker from 'faker';
 import { Bar } from 'react-chartjs-2';
 
 const Dashboard = () => {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
   const labels = [
     'January',
     'February',
@@ -21,23 +50,17 @@ const Dashboard = () => {
     datasets: [
       {
         label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: [10, 20, 20, 30, 50, 10, 12, 34, 456, 67, 89, 34],
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
     ],
   };
-
   return (
     <div className="project-management-dashboard-base-div">
       <div className="project-management-dashoard-graph-1">
-        <Bar data={data} />
+        <Bar data={data} options={options} />
       </div>
-      <div className="project-management-dashoard-graph-2"> Graph 2 </div>
+      <div className="project-management-dashoard-graph-2">Bar Graph 2</div>
       <div className="project-management-dashoard-table-1">Table</div>
       <div className="project-management-dashoard-graph-3">Graph 3</div>
       <div className="project-management-dashoard-table-2">Table 2</div>
